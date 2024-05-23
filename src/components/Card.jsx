d02fd9f718c416fea462ef1fd3cf438b0c78a828
Leandro De Los Santos Aboy <95661586+Leandelosantos@users.noreply.github.com>
2024-05-23 00:21:58 +0000

    prueba de consola

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,47 +1,47 @@
-import svg from '../images/pattern-divider-desktop.svg'
-import svgMobile from '../images/pattern-divider-mobile.svg'
-import dado from '../images/icon-dice.svg'
-import { useState } from 'react'
-import { getQuote } from '../helpers/getQuote'
-
-
+import svg from "../images/pattern-divider-desktop.svg";
+import svgMobile from "../images/pattern-divider-mobile.svg";
+import dado from "../images/icon-dice.svg";
+import { useState } from "react";
+//import { getQuote } from '../helpers/getQuote'
+import frases from "../frases.js";
 
 export const Card = () => {
+  const [newQuote, setNewQuote] = useState({});
 
-    const [newQuote, setNewQuote] = useState({});
+  // const getNewQuote = async() => {
+  //     const newQuote = await getQuote();
+  //     setNewQuote(newQuote)
+  //     return newQuote
+  // }
 
-    const getNewQuote = async() => {
-        const newQuote = await getQuote();
-        setNewQuote(newQuote)
-        return newQuote
-    }
+  const getNewQuote = () => {
+    const newQuote = frases[0];
+    console.log(newQuote);
+    setNewQuote();
+    return newQuote;
+  };
 
-    const onQuote = () => {
-        getNewQuote();
-    }
+  const onQuote = () => {
+    getNewQuote();
+  };
 
-    const { id, advice } = newQuote
-
+  const { id, advice } = newQuote;
 
   return (
     <div className="cardContainer">
-        <h6>ADVICE #{id} </h6>
-        <p>"{ advice }"</p>
-        <img 
-            className='img' 
-            srcSet={`${svgMobile} 295w, ${svg} 444`}
-            sizes="(max-width: 375px) 295px, 
-                    (min-width: 376px) 444px, 800w" 
-            src={svg} 
-            alt="foto" />
-        <div className='dadoContainer'>
-            <img 
-                className='dado' 
-                src={dado} 
-                alt="dado"
-                onClick={onQuote} />
-        </div>
-        
+      <h6>ADVICE #{id} </h6>
+      <p>"{advice}"</p>
+      <img
+        className="img"
+        srcSet={`${svgMobile} 295w, ${svg} 444`}
+        sizes="(max-width: 375px) 295px, 
+                    (min-width: 376px) 444px, 800w"
+        src={svg}
+        alt="foto"
+      />
+      <div className="dadoContainer">
+        <img className="dado" src={dado} alt="dado" onClick={onQuote} />
+      </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/frases.js
@@ -0,0 +1,8 @@
+const frases = [
+  {
+    1: "Disculpen chicos, justo windows decidio meterme un update.",
+    2: "Hoy el wifi esta terrible, estoy colgada del celu con datos.",
+  },
+];
+
+export default frases;
